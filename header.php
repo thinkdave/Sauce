9a261b4fdc850b02cc1a1b20fbfd98bdbbee9330
Mihai Olaru <pffmihai@gmail.com>
2018-11-12 15:52:44 +0200

    Change header to use the bootstrap grid (just adding the container)

--- header.php
+++ header.php
@@ -13,32 +13,34 @@
     <a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'thm' ); ?></a>
 
     <header id="site-header" class="site-header" role="banner">
-      	<div class="row">
-	        <div class="site-branding">
-				<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>"/>
-					</a>
-				</h1>
-				<?php else : ?>
-				<p class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>"/>
-					</a>
-				</p>
-				<?php endif; ?>
-	        </div> <!--/branding-->
-        	<nav id="nav" class="nav-header">
-            	<div class="responsive-menu-icon">
-            		<div class="menu-toggle">
-		            	<span class="menu-bar"></span>
-		            	<span class="menu-bar"></span>
-		            	<span class="menu-bar"></span>
+    	<div class="container">
+	      	<div class="row">
+		        <div class="site-branding">
+					<?php if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>"/>
+						</a>
+					</h1>
+					<?php else : ?>
+					<p class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>"/>
+						</a>
+					</p>
+					<?php endif; ?>
+		        </div> <!--/branding-->
+	        	<nav id="nav" class="nav-header">
+	            	<div class="responsive-menu-icon">
+	            		<div class="menu-toggle">
+			            	<span class="menu-bar"></span>
+			            	<span class="menu-bar"></span>
+			            	<span class="menu-bar"></span>
+		            	</div>
 	            	</div>
-            	</div>
-            	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => '', 'after'    => '<span class="arrow"></span>', ) ); ?>
-          	</nav>
-	    </div><!--/row-->
+	            	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => '', 'after'    => '<span class="arrow"></span>', ) ); ?>
+	          	</nav>
+		    </div><!--/row-->
+	    </div>
     </header>
     <div id="content" class="content">
