ad48a364d1bb7296192e8b3d422b75b960ede205
Mihai Olaru <pffmihai@gmail.com>
2018-11-12 17:32:20 +0200

    Footer structure, widgets area, CSS

--- footer.php
+++ footer.php
@@ -1,14 +1,33 @@
   	</div><!--/content-->
 
 	<footer class="site-footer">
-		<div class="container">
-			<div class="row">
-				<div class="colophon">
-
-				</div><!--/colophon-->
-
-			</div><!--/row-->
-		</div><!-- /container -->
+		<div class="colophon">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm">
+						<?php dynamic_sidebar( 'footer-1' ); ?>
+					</div>
+					<div class="col-sm">
+						<?php dynamic_sidebar( 'footer-2' ); ?>
+					</div>
+					<div class="col-sm">
+						<?php dynamic_sidebar( 'footer-3' ); ?>
+					</div>
+				</div><!--/row-->
+			</div><!-- /container -->
+		</div><!--/colophon-->
+		<div class="footer-utility">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm copy">
+						&copy <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>
+					</div>
+					<div class="col-sm designby">
+						<a href="https://proofbranding.com" target="_blank">Finely Crafted by Proof Branding</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 </div><!--/page-->
 <?php wp_footer(); ?>
