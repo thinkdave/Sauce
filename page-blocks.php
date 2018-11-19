a46a22ad20fdeaa67c9a24d69a06eaf592f654e2
Mihai Olaru <pffmihai@gmail.com>
2018-11-19 15:00:17 +0200

    Update page-blocks.php

--- page-blocks.php
+++ page-blocks.php
@@ -28,6 +28,9 @@ get_header(); ?>
 			elseif(get_row_layout() == 'block_columns_six') :
 			get_template_part('blocks/columns-six');
 
+		elseif(get_row_layout() == 'block_accordion') :
+			get_template_part('blocks/accordion');
+
 
 			else : endif;
 
