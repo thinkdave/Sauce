22a34e16d0ebff9a9e87c74f776ddab2beb0c13a
Mihai Olaru <pffmihai@gmail.com>
2018-11-19 11:57:36 +0200

    Add ACF fields to Block Text-Image

--- blocks/text-image.php
+++ blocks/text-image.php
@@ -1,40 +1,37 @@
 <?php
+	if(get_sub_field('custom_html_id')) {
+		$html_id = 'id="';
+		$html_id .= get_sub_field('custom_html_id');
+		$html_id .= '"';
+	} else {
+		$html_id = '';
+	}
 
-  $image_position = get_sub_field();
-
-  if($image_position === 'image-right') {
-    $text_position = 'col-lg-5 order-lg-2';
-  } else {
-    $text_position = 'col-lg-5 offset-lg-7';
-  }
-
+	if(get_sub_field('custom_html_class')) {
+		$html_class = ' ' . get_sub_field('custom_html_class');
+	} else {
+		$html_class = '';
+	}
+	
+	$a = get_sub_field('custom_html_class');
+	if (strpos($a, 'reverse') !== false) {
+		$text_position = 'col-lg-5 order-lg-2';
+		$image_position = 'image-right';
+	}else{
+		$text_position = 'col-lg-5 offset-lg-7';
+		$image_position = 'image-left';
+	}
 ?>
 
 
 <section class="block text-image">
   <div class="text-image-wrap">
     <div class="container">
-      <div class="text-image-img image-right col-lg-6" style="background-image: url(<?php bloginfo('template_url'); ?>/img/img2.jpg);"></div>
+    	<?php $bgImage = get_sub_field('image'); ?>
+      <div class="text-image-img col-lg-6 <?php echo $image_position; ?>" style="background-image: url(<?php echo $bgImage['url']; ?>)"></div>
       <div class="row">
-        <div class="text-image-content col-lg-5 order-lg-2">
-          <h2>A brief history</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit eget leo tempus placerat. Quisque cursus pellentesque neque sit amet elementum. Ut iaculis velit erat, et scelerisque velit tincidunt non. Curabitur eget nisl a mi viverra finibus.</p>
-          <p>Proin purus ex, porttitor at purus ut, vestibulum viverra lorem. Quisque suscipit tellus ut augue maximus, id blandit erat sodales. Nullam sed mi purus.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="block text-image">
-  <div class="text-image-wrap">
-    <div class="container">
-      <div class="text-image-img image-left col-lg-6" style="background-image: url(<?php bloginfo('template_url'); ?>/img/img2.jpg);"></div>
-      <div class="row">
-        <div class="text-image-content col-lg-5 offset-lg-7">
-          <h2>A brief history</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus blandit eget leo tempus placerat. Quisque cursus pellentesque neque sit amet elementum. Ut iaculis velit erat, et scelerisque velit tincidunt non. Curabitur eget nisl a mi viverra finibus.</p>
-          <p>Proin purus ex, porttitor at purus ut, vestibulum viverra lorem. Quisque suscipit tellus ut augue maximus, id blandit erat sodales. Nullam sed mi purus.</p>
+        <div class="text-image-content <?php echo $text_position; ?>">
+          	<?php the_sub_field('content');?>
         </div>
       </div>
     </div>
